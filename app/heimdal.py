69b5cc2bd3436b6a91c05796829540b9f96e8f3b
Adam Christie <fractos@gmail.com>
2017-10-09 23:17:06 +0100

    latest.

--- app/heimdal.py
+++ app/heimdal.py
@@ -1,4 +1,13 @@
 import os
 import quickproxy
+import json
 import settings
 
+def callback(request):
+    request.host = settings.HOST
+    request.port = settings.PORT
+    return request
+
+rules = json.loads(open(settings.RULES_FILE).read())
+
+quickproxy.run_proxy(port=settings.PORT, req_callback=callback)
--- app/settings.py
+++ app/settings.py
@@ -2,3 +2,4 @@ import os
 
 HOST = os.environ.get('HOST')
 PORT = int(os.environ.get('PORT'))
+RULES_FILE = os.environ.get('RULES_FILE')
