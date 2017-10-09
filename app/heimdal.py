b0aba0634ee560e45c699487869ea74783b3d946
Adam Christie <fractos@gmail.com>
2017-10-09 23:14:44 +0000

    fix for run script. adding log to request callback.

--- app/heimdal.py
+++ app/heimdal.py
@@ -1,11 +1,15 @@
 import os
 import quickproxy
 import json
+import logging
 import settings
 
 def callback(request):
     request.host = settings.HOST
     request.port = settings.PORT
+
+    print('request received: ' + request.method + ' ' + request.path)
+
     return request
 
 rules = json.loads(open(settings.RULES_FILE).read())
--- run_heimdal.sh
+++ run_heimdal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws s3 cp $(RULES_S3_URI) /opt/heimdal/rules.json
+aws s3 cp $RULES_S3_URI /opt/heimdal/rules.json
 
 python -u /opt/heimdal/heimdal.py
